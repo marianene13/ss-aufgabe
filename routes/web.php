f9abfd375ac41933aaffe13348506a96de856e02
Marian Ene <marianionut.ene@gmail.com>
2024-11-07 14:21:49 +0100

    add routes for controller actions

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
+use App\Http\Controllers\PlaylistController;
 use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\SongController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -12,6 +14,13 @@ Route::get('/dashboard', function () {
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
+    Route::get('/playlists', [PlaylistController::class, 'index'])->name('playlists.index');
+    Route::POST('/playlists/add', [PlaylistController::class, 'add'])->name('playlists.add');
+    Route::get('/playlists/{id}', [PlaylistController::class, 'show'])->name('playlists.show');
+    Route::delete('/playlists/{id}/removeSong', [PlaylistController::class, 'removeSong'])->name('playlists.removeSong');
+    Route::post('/playlists/{id}/loadSongs', [PlaylistController::class, 'loadSongs'])->name('playlists.loadSongs');
+    Route::get('/songs', [SongController::class, 'index'])->name('songs.index');
+    Route::get('/songs/{id}', [SongController::class, 'show'])->name('songs.show');
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
